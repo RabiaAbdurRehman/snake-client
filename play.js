fadf94c5fcab29796696dade014872c3ccab5786
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-06-15 16:51:30 -0600

    set up first event.

---
+++ play.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+//Establish a connection.
+const connect = function(){
+    const conn = net.createConnection({
+        host: "localhost",
+        port:  50541
+    });
+
+    //Interpret the incoming data as a text.
+    conn.setEncoding("utf8");
+
+    //events:
+    conn.on("data", (data) => {
+
+        console.log(data);
+
+    });
+
+    return conn;
+
+};
+
+console.log("connecting.....");
+connect();
