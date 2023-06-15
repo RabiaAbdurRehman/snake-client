a55757ad1fba99d627495dbcd0223e6a6ab9f4fc
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-06-15 17:28:52 -0600

    Adding export and import options

--- play.js
+++ play.js
@@ -1,25 +1,6 @@
-const net = require("net");
 
-//Establish a connection.
-const connect = function(){
-    const conn = net.createConnection({
-        host: "localhost",
-        port:  50541
-    });
-
-    //Interpret the incoming data as a text.
-    conn.setEncoding("utf8");
-
-    //events:
-    conn.on("data", (data) => {
-
-        console.log(data);
-
-    });
-
-    return conn;
-
-};
+//importing....
+const { connect } = require("./client");
 
 console.log("connecting.....");
 connect();
