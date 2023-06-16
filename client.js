18cf546ec31f6c45724e1ab9297cf89af6df29e4
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-06-16 01:50:21 -0600

    Updating

--- client.js
+++ client.js
@@ -1,52 +1,26 @@
 const net = require("net");//build in library in node.
 const { IP, PORT } = require("./constants");
-
 //Establish a connection.
 const connect = function() {
     const conn = net.createConnection({
         host: IP,
         port: PORT
     });
-
     //Interpret the incoming data as a text.
     conn.setEncoding("utf8");
-
     //events:
     //keyword connect for user.
     conn.on("connect", (connects) => {
-
-
         console.log("connected.....");
         conn.write("Name: Rab");
-
-       // it was moved up so quick thats why it didnt show initial.
-        // setTimeout(() => {
-        //     conn.write("Move: up");
-
-        // }, 2000);
-        // setTimeout(() => {
-        //     conn.write("Move: left");
-
-        // }, 3000);
-
-
     });
     //to get from server, we use keyword data.
     conn.on("data", (dataFromServer) => {
-
-
         console.log(dataFromServer);
-
     });
-
-
     return conn;
-
 };
 // exporting....
 module.exports = {
-
     connect,
-
-
 };
--- play.js
+++ play.js
@@ -1,9 +1,6 @@
-
 //importing....
 const { connect } = require("./client");
-
-const {setupInput} = require("./input");
-
+const { setupInput } = require("./input");
 console.log("connecting.....");
 let conn = connect();
 setupInput(conn);
