3eee42a7b9d713ed3ee8014f7698a5e678b4e029
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-06-16 01:51:03 -0600

    Updating

--- input.js
+++ input.js
@@ -1,19 +1,4 @@
-
 let connection; //connection between your input module and the server.
-
-// const handleKeyPress = function(event){
-//     if(event.key === 1){
-//        /*  // Replace `sendData` with the actual function name and 'Your message here' with the actual message
-//     sendDataSay: Your message here");*/
-//         connection.write("Done")}
-//     if(event.key=== 2){
-//         connection.write("why")
-//     }
-//     if(event.key=== 3){
-//         connection.write("how")
-//     }
-// }
-
 const handleUserInput = function(key) {
     let message1 = "why";
     let message2 = "why";
@@ -35,7 +20,7 @@ const handleUserInput = function(key) {
     }
     if (key === '1') {
         // connection.write(message1);it wont work with connection.write.
-       connection.write(`Say: ${message1}`); // why cant I just write "Done" or "Why"?
+        connection.write(`Say: ${message1}`); // why cant I just write "Done" or "Why"?
     }
     if (key === '2') {
         connection.write(`Say: ${message2}`);
@@ -45,25 +30,19 @@ const handleUserInput = function(key) {
     }
 };
 const setupInput = function(conn) {
-    connection = conn;//?
+    connection = conn;
     const stdin = process.stdin;
     stdin.setRawMode(true);
     stdin.setEncoding("utf8");
     stdin.resume();
     stdin.on("data", handleUserInput);
-
-    // document.addEventListener('keydown', handleKeyPress)
-
     //on: listening the input from the terminal. node defines certain events:
     //stdin is giving the program the access to stream of input.register (listen to something, telling node some kind of event)
     //an even handler(second argument is the handler like what do you want to do.).
     // stdout comes when we want to put some output for user to see something
     return stdin;
 };
-
-
 // Stores the active TCP connection object.
-
 module.exports = {
     setupInput,
 
